0118467006795a5c79bc45e3af6ad4ec1c62b6d3
syedsolarenergy <sales@syedsolarenergy.com>
2025-08-08 01:44:36 +0500

    AV

--- src/pages/Quotationsoftware.js
+++ src/pages/Quotationsoftware.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { supabase } from './supabaseClient';
+import { supabase } from "../supabaseClient";
 
 const QuotationSoftware = () => {
   // State variables
